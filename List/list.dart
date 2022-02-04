ed014aec02c48a9ff5f4004ffa9dc02a7e2432b1
Tawhid-Komol <tawhidflutter21@gmail.com>
2022-02-05 02:53:37 +0600

    first commit

---
+++ List/list.dart
@@ -0,0 +1,16 @@
+void main() {
+  List<String> fruits = [
+    'Apple',
+    'Banana',
+    'PineApple',
+    'Orange',
+  ];
+  //print(fruits[2]);
+
+  //print(fruits.indexOf('Orange'));
+
+  //fruits.add('Mango');
+
+  fruits.insert(1, 'Mango');
+  print(fruits);
+}
